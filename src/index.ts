ea160e4f1f5374fb715abd0c69370f25c9e9a08c
Toru Kobayashi <koba0004@gmail.com>
2020-10-15 17:05:35 +0900

    refactor: implement WebpackPluginInstance

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Compiler } from "webpack";
+import { Compiler, WebpackPluginInstance } from "webpack";
 import { validate, ECMAVersion } from "./validate";
 
-export class ECMAVersionValidatorPlugin /* implements ??? */ {
+export class ECMAVersionValidatorPlugin implements WebpackPluginInstance {
   name: string;
   ecmaVersion: ECMAVersion;
   test: RegExp;
